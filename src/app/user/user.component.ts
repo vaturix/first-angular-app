21088e9175c9b60f212a0ecb4974423a116f9330
İsa Öztürk <isa.ozturk@csb.gov.tr>
2025-02-04 14:45:01 +0300

    click and change avatar

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DUMMY_USERS } from '../dummy-users';
+const randomIndex= Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+selectedUser = DUMMY_USERS[randomIndex];
+
+get imagePath() {
+  return `assets/users/${this.selectedUser.avatar}`;
+}
+onSelectUser() {
+const randomIndex= Math.floor(Math.random() * DUMMY_USERS.length);
+
+  this.selectedUser = DUMMY_USERS[randomIndex];
+
+}
 
 }
