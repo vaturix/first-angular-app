c5aaab86d911ca5a7f2e48d380f6b273ff41ea05
İsa Öztürk <isa.ozturk@csb.gov.tr>
2025-02-08 18:51:22 +0300

    submit data

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,27 @@
 import { Component,EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
 @Output() cancel = new EventEmitter<void>();
+@Output() add = new EventEmitter<NewTaskData>();
+enteredTitle = '';
+enteredSummary = '';
+enteredDueDate = '';
   onCancel() {
     this.cancel.emit();
+}
+onSubmit() {
+this.add.emit({
+  title: this.enteredTitle,
+  summary: this.enteredSummary,
+  date: this.enteredDueDate,
+});
 }
 }
