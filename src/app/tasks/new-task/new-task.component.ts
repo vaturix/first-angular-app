6c2f886921e211301190aa4ee6c1a1d45f8dd3c9
İsa Öztürk <isa.ozturk@csb.gov.tr>
2025-02-10 07:04:58 +0300

    done

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component,EventEmitter, Output } from '@angular/core';
+import { Component,EventEmitter, Output,inject, Input} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -9,19 +10,21 @@ import { NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-@Output() cancel = new EventEmitter<void>();
-@Output() add = new EventEmitter<NewTaskData>();
+  @Input({required: true}) userId!: string;
+@Output() close = new EventEmitter<void>();
 enteredTitle = '';
 enteredSummary = '';
 enteredDueDate = '';
+private tasksService = inject(TasksService)
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
 }
 onSubmit() {
-this.add.emit({
-  title: this.enteredTitle,
-  summary: this.enteredSummary,
-  date: this.enteredDueDate,
-});
+this.tasksService.addTask({
+title: this.enteredTitle,
+summary: this.enteredSummary,
+date: this.enteredDueDate
+}, this.userId);
+this.close.emit();
 }
 }
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,18 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
 @Input({required: true}) task!: Task;
-@Output() complete = new EventEmitter<string>();
+private tasksService = inject(TasksService);
 
 onCompleteTask() {
-this.complete.emit(this.task.id);
+this.tasksService.removeTask(this.task.id);
 }
 }
